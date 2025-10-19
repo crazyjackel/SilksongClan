78962ae5ce0daeb42a30ff4b3d7c7e86ee9ed621
Jackson Levitt <jackel1020@gmail.com>
2025-10-19 15:58:11 -0400

    Fix CardTraitScaleDamageFullSilk

--- Silk_Song_Clan.Plugin/CardTraits/CardTraitScaleDamageFullSilk.cs
+++ Silk_Song_Clan.Plugin/CardTraits/CardTraitScaleDamageFullSilk.cs
@@ -1,32 +1,38 @@
-namespace Silk_Song_Clan.Plugin{
-	public override PropDescriptions CreateEditorInspectorDescriptions()
-	{
-		PropDescriptions propDescriptions = new PropDescriptions();
-		string fieldName = CardTraitFieldNames.ParamFloat.GetFieldName();
-		propDescriptions[fieldName] = new PropDescription("Stacks Per Stat", "", null, false);
-		return propDescriptions.Add(base.CreateStatValueDataInspectorDescriptions());
-	}
-	public override int OnApplyingBuffDamageToUnit(CardTraitState.ApplyingDamageParameters parameters, ICoreGameManagers coreGameManagers)
-	{
-		return this.GetExtraDamage(coreGameManagers.GetCardStatistics());
-	}
+using TrainworksReloaded.Base.Card;
+using TrainworksReloaded.Core.Interfaces;
+using TrainworksReloaded.Core.Extensions;
+using TrainworksReloaded.Core;
+using TrainworksReloaded.Core.Enum;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 
-	private int GetExtraDamage(CardStatistics cardStats)
+namespace Silk_Song_Clan.Plugin
+{
+	public class CardTraitScaleDamageFullSilk : CardTraitState
 	{
-        return 0;
-		int statValue = cardStats.GetStatValue(base.StatValueData);
-		return base.GetParamInt() * statValue;
-	}
-
-	[NullableContext(2)]
-	[return: Nullable(1)]
-	public override string GetCurrentEffectText(CardStatistics cardStatistics, SaveManager saveManager, RelicManager relicManager)
-	{
-		if (cardStatistics != null && cardStatistics.GetStatValueShouldDisplayOnCardNow(base.StatValueData))
+		public override PropDescriptions CreateEditorInspectorDescriptions()
 		{
-			int extraDamage = this.GetExtraDamage(cardStatistics);
-			return "CardTraitScalingBuffDamage_CurrentScaling_CardText".Localize(new LocalizedIntegers(new int[] { extraDamage }));
+			PropDescriptions propDescriptions = new PropDescriptions();
+			string fieldName = CardTraitFieldNames.ParamFloat.GetFieldName();
+			propDescriptions[fieldName] = new PropDescription("Damage Multiplier", "", null, false);
+			return propDescriptions.Add(base.CreateStatValueDataInspectorDescriptions());
 		}
-		return string.Empty;
+		private SilkManager GetSilkManager()
+		{
+			var container = Railend.GetContainer();
+			return container.GetInstance<SilkManager>();
+		}
+
+        public override int OnApplyingDamage(ApplyingDamageParameters damageParams, ICoreGameManagers coreGameManagers)
+        {
+			int num = damageParams.damage;
+			var silkManager = GetSilkManager();
+			if (silkManager != null && silkManager.GetCurrentSilk() == silkManager.GetMaxSilk())
+			{
+				num = (int)(num * this.GetParamFloat());
+			}
+			return num;
+        }
 	}
 }
