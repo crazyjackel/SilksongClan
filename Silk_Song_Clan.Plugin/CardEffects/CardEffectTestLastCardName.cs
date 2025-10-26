e658b326414239e5c799eb017b4801bb5cc4f5b3
Jackson Levitt <jackel1020@gmail.com>
2025-10-26 18:35:21 -0400

    fix some flea stuff and Test Effects for Combo

--- Silk_Song_Clan.Plugin/CardEffects/CardEffectTestLastCardName.cs
+++ Silk_Song_Clan.Plugin/CardEffects/CardEffectTestLastCardName.cs
@@ -17,12 +17,13 @@ namespace Silk_Song_Clan.Plugin
         {
             var cardManager = coreGameManagers.GetCardManager();
             var cardsPlayedThisTurn = (List<CardState>)AccessTools.Field(typeof(CardManager), "cardsPlayedThisTurn").GetValue(cardManager);
-            var lastCard = cardsPlayedThisTurn.LastOrDefault();
-            if (lastCard == null)
+            var cardsPlayedThisTurnCount = cardsPlayedThisTurn.Count;
+            if (cardsPlayedThisTurnCount < 2)
             {
-                Plugin.Logger.LogInfo("No last card played this turn");
+                Plugin.Logger.LogInfo("Not enough cards played this turn");
                 return false;
             }
+            var lastCard = cardsPlayedThisTurn[cardsPlayedThisTurnCount - 2]; // Since we want the second to last card, we subtract 2 from the count
             var cardName = cardEffectState.GetParamStr();
             var fullCardName = cardName.ToId(MyPluginInfo.PLUGIN_GUID, TemplateConstants.Card);
             Plugin.Logger.LogInfo("Testing last card name: " + lastCard.GetAssetName());
